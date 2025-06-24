22ceac417f26a29de60c3962021d1079505be372
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-06-24 20:42:47 +0200

    Fix LNbits account creation

--- src/matrix_bot/business_logic.rs
+++ src/matrix_bot/business_logic.rs
@@ -464,13 +464,13 @@ impl BusinessLogicContext {
             let admin_id = Uuid::new_v4().to_string();
             let user_name = matrix_id;
             let email = "";
-            let password = "";
+            let password = Uuid::new_v4().to_string();
 
             let create_user_args = CreateUserArgs::new(wallet_name.as_str(),
                                                        admin_id.as_str(),
                                                        user_name,
                                                        email,
-                                                       password);
+                                                       password.as_str());
             let result = self.lnbits_client.create_user_with_initial_wallet(&create_user_args).await;
             match  result {
                 Ok(result) => {
