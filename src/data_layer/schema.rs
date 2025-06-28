aa89d44698dba1ce7cd0a5c0f8e4f08f513e98e1
wario_is_here <117512622+warioishere@users.noreply.github.com>
2025-06-28 14:34:08 +0200

    Update schema.rs

--- src/data_layer/schema.rs
+++ src/data_layer/schema.rs
@@ -1,19 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    matrix_id_2_lnbits_id (matrix_id) {
-        matrix_id -> Text,
-        lnbits_id -> Text,
-        lnbits_admin -> Text,
-        date_created -> Text,
-    }
-}
-
-diesel::table! {
     client_auth (id) {
         id -> Nullable<Integer>,
         access_token -> Text,
         device_id -> Text,
+    }
+}
+
+diesel::table! {
+    dm_rooms (matrix_id) {
+        matrix_id -> Nullable<Text>,
+        room_id -> Text,
     }
 }
 
@@ -28,6 +26,15 @@ diesel::table! {
 }
 
 diesel::table! {
+    matrix_id_2_lnbits_id (matrix_id) {
+        matrix_id -> Text,
+        lnbits_id -> Text,
+        lnbits_admin -> Text,
+        date_created -> Text,
+    }
+}
+
+diesel::table! {
     matrix_store (id) {
         id -> Nullable<Integer>,
         state -> Binary,
@@ -35,9 +42,10 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    dm_rooms (matrix_id) {
-        matrix_id -> Text,
-        room_id -> Text,
-    }
-}
+diesel::allow_tables_to_appear_in_same_query!(
+    client_auth,
+    dm_rooms,
+    ln_addresses,
+    matrix_id_2_lnbits_id,
+    matrix_store,
+);
