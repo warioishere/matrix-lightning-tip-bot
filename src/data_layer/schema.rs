b25b8429209269c283ca3a81eb28e13d4e02cc1d
warioishere <mario.hofmann@yourdevice.ch>
2025-06-28 22:31:46 +0000

    schema.rs changes

--- src/data_layer/schema.rs
+++ src/data_layer/schema.rs
@@ -1,14 +1,11 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    matrix_id_2_lnbits_id (matrix_id) {
-        matrix_id -> Text,
-        lnbits_id -> Text,
-        lnbits_admin -> Text,
-        date_created -> Text,
+    dm_rooms (matrix_id) {
+        matrix_id -> Nullable<Text>,
+        room_id -> Text,
     }
 }
-
 
 diesel::table! {
     ln_addresses (id) {
@@ -21,6 +18,15 @@ diesel::table! {
 }
 
 diesel::table! {
+    matrix_id_2_lnbits_id (matrix_id) {
+        matrix_id -> Text,
+        lnbits_id -> Text,
+        lnbits_admin -> Text,
+        date_created -> Text,
+    }
+}
+
+diesel::table! {
     matrix_store (id) {
         id -> Nullable<Integer>,
         state -> Binary,
@@ -28,9 +34,9 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    dm_rooms (matrix_id) {
-        matrix_id -> Text,
-        room_id -> Text,
-    }
-}
+diesel::allow_tables_to_appear_in_same_query!(
+    dm_rooms,
+    ln_addresses,
+    matrix_id_2_lnbits_id,
+    matrix_store,
+);
